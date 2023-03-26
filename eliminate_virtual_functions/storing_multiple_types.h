761d250481a8ee0d73adea545a8a1170b3a097b0
mirinta <mirinta@163.com>
2023-03-26 12:30:29 +0800

    minor changes

--- eliminate_virtual_functions/storing_multiple_types.h
+++ eliminate_virtual_functions/storing_multiple_types.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <concepts>
 #include <tuple>
 #include <vector>
 
@@ -29,9 +30,16 @@ private:
     std::tuple<std::vector<Ts>...> data;
 };
 
-inline void usage() {
-    struct T1 { int func(); };
-    struct T2 { int func(); };
+inline void usage()
+{
+    struct T1
+    {
+        int func();
+    };
+    struct T2
+    {
+        int func();
+    };
 
     using StoreT1AndT2 = MultiTypesWrapper<T1, T2>;
     StoreT1AndT2 store{};
