f3b41776d86df51aa0ecffd79790a6375c3b50fb
mirinta <mirinta@163.com>
2023-03-29 00:45:44 +0800

    feat: type_list

--- template_metaprogramming/trait_type_list.h
+++ template_metaprogramming/trait_type_list.h
@@ -4,23 +4,21 @@
 #include <type_traits>
 
 /**
- * compile-time operations:
- * 1. length
- * 2. append Ts...
- * 3. prepend Ts...
- * 4. get at given index
- * 6. has T
- * 7. index of T
- * 8. erase T
- * 9. replace T to U
- * 10. remove duplicates
- * 11. reverse
- * 12. enumerate?
- * 13. sort?
- * 14. merge elements of L2 to L1
- * https://github.com/lipk/cpp-typelist/blob/master/tests.cpp
- * https://github.com/Pseudomanifold/TypeSwitch/blob/master/type_list.hh
- * https://www.cs.fsu.edu/~lacher/lectures/Output/loki3/index.html?$$$slide01.html$$$
+ * Compile-time operations of @see type_list:
+ *  1. size of the list;
+ *  2. append given type T;
+ *  3. prepend given type T;
+ *  4. merge given lists;
+ *  6. reverse the order of the types;
+ *  7. get the type at given index I;
+ *  8. get the index of the first occurrence of given type T in the list (-1 if not found);
+ *  9. check if the list contains given type T;
+ * 10. remove the type at given index I;
+ * 11. replace the type at given index I with given type T;
+ *
+ * TODO: swap? extract? enumera? sort?
+ *
+ * Reference: https://github.com/lipk/cpp-typelist/blob/master/typelist.hpp
  */
 template <typename...>
 struct type_list;
@@ -37,81 +35,136 @@ struct is_type_list<type_list<Ts...>> : public std::true_type
 };
 
 template <typename T>
-concept TypeList = is_type_list<T>::value;
+constexpr auto is_type_list_v = is_type_list<T>::value;
 
 template <typename... Ts>
-inline constexpr size_t type_count_v = sizeof...(Ts);
-
-template <typename... Ts>
-concept NonEmpty = type_count_v<Ts...> > 0;
-
-template <size_t Index, typename... Ts>
-concept ValidIndex = Index < type_count_v<Ts...>;
-
-template <typename... Ts>
-requires NonEmpty<Ts...> struct list_head;
-
-template <typename T0, typename... Ts>
-struct list_head<T0, Ts...> : public std::type_identity<T0>
+struct type_count : public std::integral_constant<size_t, sizeof...(Ts)>
 {
 };
 
 template <typename... Ts>
-using list_head_t = typename list_head<Ts...>::type;
+constexpr auto type_count_v = type_count<Ts...>::value;
 
-template <typename... Ts>
-requires NonEmpty<Ts...> struct list_tail;
-
-template <typename T0, typename... Ts>
-struct list_tail<T0, Ts...> : public std::type_identity<type_list<Ts...>>
-{
-};
-
-template <typename... Ts>
-using list_tail_t = typename list_tail<Ts...>::type;
-
-template <typename, TypeList>
+template <typename L1, typename... L2>
 struct merge_lists;
 
-template <typename... Ts, typename... Us>
-struct merge_lists<type_list<Ts...>, type_list<Us...>>
-{
-    using type = type_list<Ts..., Us...>;
-};
+template <typename L1, typename... L2>
+using merge_lists_t = typename merge_lists<L1, L2...>::type;
 
-template <size_t Index, typename... Ts>
-requires ValidIndex<Index, Ts...> struct get_type
+template <typename... Ts, typename... Us, typename... Ls>
+struct merge_lists<type_list<Ts...>, type_list<Us...>, Ls...>
 {
-    using type = typename list_tail_t<Ts...>::template get<Index - 1>;
+    using type = merge_lists_t<type_list<Ts..., Us...>, Ls...>;
 };
 
 template <typename... Ts>
-struct get_type<0, Ts...>
+struct merge_lists<type_list<Ts...>>
 {
-    using type = list_head_t<Ts...>;
+    using type = type_list<Ts...>;
 };
 
-template <typename T, size_t Index, typename... Ts>
-struct type_index : public std::integral_constant<int64_t, -1>
+template <size_t I, typename... Ts>
+struct get_nth_type;
+
+template <size_t I, typename... Ts>
+using get_nth_type_t = typename get_nth_type<I, Ts...>::type;
+
+template <typename Head, typename... Tail>
+struct get_nth_type<0, Head, Tail...>
+{
+    using type = Head;
+};
+
+template <size_t I, typename Head, typename... Tail>
+struct get_nth_type<I, Head, Tail...>
+{
+    using type = get_nth_type_t<I - 1, Tail...>;
+};
+
+template <typename T, size_t I, typename... Ts>
+struct type_index;
+
+template <typename T, size_t I, typename... Ts>
+constexpr auto type_index_v = type_index<T, I, Ts...>::value;
+
+template <typename T, size_t I>
+struct type_index<T, I> : public std::integral_constant<int64_t, -1>
 {
 };
 
-template <typename T, size_t Index, typename... Ts>
-struct type_index<T, Index, T, Ts...> : public std::integral_constant<int64_t, Index>
+template <typename T, size_t I, typename... Ts>
+struct type_index<T, I, T, Ts...> : public std::integral_constant<int64_t, I>
 {
 };
 
-template <typename T, size_t Index, typename T0, typename... Ts>
-struct type_index<T, Index, T0, Ts...>
-    : public std::integral_constant<int64_t, type_index<T, Index + 1, Ts...>::value>
+template <typename T, size_t I, typename Head, typename... Tail>
+struct type_index<T, I, Head, Tail...>
+    : public std::integral_constant<int64_t, type_index_v<T, I + 1, Tail...>>
 {
+};
+
+template <size_t I, typename... Ts>
+struct remove_nth_type;
+
+template <size_t I, typename... Ts>
+using remove_nth_type_t = typename remove_nth_type<I, Ts...>::type;
+
+template <size_t I, typename Head, typename... Tail>
+struct remove_nth_type<I, Head, Tail...>
+{
+    using type = merge_lists_t<type_list<Head>, remove_nth_type_t<I - 1, Tail...>>;
+};
+
+template <typename Head, typename... Tail>
+struct remove_nth_type<0, Head, Tail...>
+{
+    using type = type_list<Tail...>;
+};
+
+template <size_t I, typename T, typename... Ts>
+struct replace_nth_type;
+
+template <size_t I, typename T, typename... Ts>
+using replace_nth_type_t = typename replace_nth_type<I, T, Ts...>::type;
+
+template <size_t I, typename T, typename Head, typename... Tail>
+struct replace_nth_type<I, T, Head, Tail...>
+{
+    using type = merge_lists_t<type_list<Head>, replace_nth_type_t<I - 1, T, Tail...>>;
+};
+
+template <typename T, typename Head, typename... Tail>
+struct replace_nth_type<0, T, Head, Tail...>
+{
+    using type = type_list<T, Tail...>;
+};
+
+template <typename... Ts>
+struct reverse_list
+{
+    using type = type_list<>;
+};
+
+template <typename... Ts>
+using reverse_list_t = typename reverse_list<Ts...>::type;
+
+template <typename Head, typename... Tail>
+struct reverse_list<Head, Tail...>
+{
+    using type = merge_lists_t<reverse_list_t<Tail...>, type_list<Head>>;
 };
 } // namespace impl
+
+template <typename T>
+concept TypeList = impl::is_type_list_v<T>;
+
+template <typename L, size_t Is>
+concept ValidIndex = TypeList<L> && (Is < L::size);
 
 template <typename... Ts>
 struct type_list
 {
-    static constexpr auto length = impl::type_count_v<Ts...>;
+    static constexpr auto size = impl::type_count_v<Ts...>;
 
     template <typename T>
     using append = type_list<Ts..., T>;
@@ -119,17 +172,25 @@ struct type_list
     template <typename T>
     using prepend = type_list<T, Ts...>;
 
-    template <typename L>
-    using merge = typename impl::merge_lists<type_list<Ts...>, L>::type;
+    template <TypeList L, TypeList... Ls>
+    using merge = impl::merge_lists_t<type_list, L, Ls...>;
 
-    template <size_t Index>
-    using get = typename impl::get_type<Index, Ts...>::type;
+    using reverse = impl::reverse_list_t<Ts...>;
+
+    template <size_t I>
+    requires ValidIndex<type_list, I> using at = impl::get_nth_type_t<I, Ts...>;
 
     template <typename T>
-    static constexpr auto find = impl::type_index<T, 0, Ts...>::value;
+    static constexpr auto indexOf = impl::type_index_v<T, 0, Ts...>;
 
     template <typename T>
-    static constexpr auto has = find<T> >= 0;
+    static constexpr auto contains = indexOf<T> >= 0;
+
+    template <size_t I>
+    requires ValidIndex<type_list, I> using removeAt = impl::remove_nth_type_t<I, Ts...>;
+
+    template <size_t I, typename T>
+    requires ValidIndex<type_list, I> using replaceAt = impl::replace_nth_type_t<I, T, Ts...>;
 };
 
 /// test
@@ -137,9 +198,9 @@ using L0 = type_list<>;
 using L1 = type_list<int>;
 using L2 = type_list<long, double>;
 
-static_assert(0 == L0::length);
-static_assert(1 == L1::length);
-static_assert(2 == L2::length);
+static_assert(0 == L0::size);
+static_assert(1 == L1::size);
+static_assert(2 == L2::size);
 
 static_assert(std::is_same_v<L1, L0::append<int>>);
 static_assert(std::is_same_v<L1, L0::prepend<int>>);
@@ -147,19 +208,30 @@ static_assert(std::is_same_v<type_list<int, long, double>, L2::prepend<int>>);
 static_assert(std::is_same_v<type_list<long, double, int>, L2::append<int>>);
 
 static_assert(std::is_same_v<L0, L0::merge<L0>>);
-static_assert(std::is_same_v<L1, L0::merge<L1>>);
+static_assert(std::is_same_v<L1, L0::merge<L0, L0, L1>>);
+static_assert(std::is_same_v<type_list<int, int, long, double>, L0::merge<L1, L1, L2>>);
 
-static_assert(std::is_same_v<int, L1::get<0>>);
-static_assert(std::is_same_v<double, L2::get<1>>);
+static_assert(std::is_same_v<int, L1::at<0>>);
+static_assert(std::is_same_v<double, L2::at<1>>);
 
-static_assert(-1 == L0::find<int>);
-static_assert(-1 == L1::find<double>);
-static_assert(0 == L1::find<int>);
-static_assert(1 == L2::find<double>);
-static_assert(2 == type_list<int, long, double>::find<double>);
+static_assert(-1 == L0::indexOf<int>);
+static_assert(-1 == L1::indexOf<double>);
+static_assert(0 == L1::indexOf<int>);
+static_assert(1 == L2::indexOf<double>);
+static_assert(2 == type_list<int, long, double>::indexOf<double>);
 
-static_assert(not L0::has<int>);
-static_assert(not L1::has<double>);
-static_assert(L1::has<int>);
-static_assert(L2::has<double>);
-static_assert(type_list<int, long, double>::has<double>);
+static_assert(not L0::contains<int>);
+static_assert(not L1::contains<double>);
+static_assert(L1::contains<int>);
+static_assert(L2::contains<double>);
+static_assert(type_list<int, long, double>::contains<double>);
+
+static_assert(std::is_same_v<L0, L1::removeAt<0>>);
+static_assert(std::is_same_v<L2, L2::merge<L1>::removeAt<L2::size>>);
+
+static_assert(std::is_same_v<L2::removeAt<1>, L1::replaceAt<0, long>>);
+static_assert(std::is_same_v<type_list<long, size_t>, L2::replaceAt<1, size_t>>);
+
+static_assert(std::is_same_v<L0, L0::reverse>);
+static_assert(std::is_same_v<L1, L1::reverse>);
+static_assert(std::is_same_v<L2, L2::reverse::reverse>);
