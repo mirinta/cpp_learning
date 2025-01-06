26b27cc3a8ec05df7fbf1b769daa6cadc6472e49
mirinta <mirinta@163.com>
2025-01-06 16:28:51 +0800

    Create main.cpp

---
+++ software_design/strategy_pattern/template_version/main.cpp
@@ -0,0 +1,18 @@
+#include "shapes.h"
+
+void drawAllShapes(const std::vector<std::unique_ptr<Shape>>& shapes)
+{
+    for (const auto& shape : shapes) {
+        shape->draw();
+    }
+}
+
+int main()
+{
+    std::vector<std::unique_ptr<Shape>> shapes;
+    shapes.emplace_back(std::make_unique<Circle>(1.23, std::make_unique<OpenGLCircleStrategy>()));
+    shapes.emplace_back(std::make_unique<Circle>(2.34, std::make_unique<MetalCircleStrategy>()));
+    shapes.emplace_back(std::make_unique<Square>(3.45, std::make_unique<OpenGLSquareStrategy>()));
+    shapes.emplace_back(std::make_unique<Square>(4.56, std::make_unique<MetalSquareStrategy>()));
+    drawAllShapes(shapes);
+}
